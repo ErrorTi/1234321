d6359589c5e6d5f3f6c211cbf8be15b0784e5a00
323 <323@323-15>
2025-04-17 13:41:30 +0300

    Добавьте файлы проекта.

---
+++ ClassLibrary1/StClass.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary1
+{
+    public class StClass
+    {
+        public static string AddA(string str)
+        {
+            return "A_" + str;
+        }
+    }
+}
---
+++ UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using ClassLibrary1;
+using System.Security.Cryptography.X509Certificates;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            string s = "CMD";
+            string a = "A_" + s;
+            Assert.AreEqual(a, StClass.AddA(s));
+        }       
+    }
+}
+
